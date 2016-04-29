135236eca1aeac99a1a71f95af29bccdc4cd5cd9
jlchereau <jlchereau@msn.com>
2016-04-29 11:51:20 +0100

    Added autoHint:false and upgraded to latest releases https://github.com/kidoju/Kidoju-WebFonts/issues/5

--- gruntfile.js
+++ gruntfile.js
@@ -12,30 +12,31 @@ module.exports = function(grunt){
 
     grunt.initConfig({
 
-        //Referencing package.json might be useful to embed a copyright or version number
-        //grunt-webfont does not seem to allow it at this stage
-        //We might consider using http://www.microsoft.com/typography/PropertiesEditor.mspx
+        // Referencing package.json might be useful to embed a copyright or version number
+        // grunt-webfont does not seem to allow it at this stage
+        // We might consider using http://www.microsoft.com/typography/PropertiesEditor.mspx
         pkg: grunt.file.readJSON('package.json'),
 
-        //https://github.com/sapegin/grunt-webfont
+        // https://github.com/sapegin/grunt-webfont
 webfont: {
     icons: {
         src: 'svg/*.svg',
         dest: 'dist/fonts',
         options: {
             font: 'kidoju',
-            hashes: false, //do not add random string at the end of the font file names (used to avoid caching when changing version)
+            autoHint: false, // https://github.com/sapegin/grunt-webfont/issues/329#issuecomment-215626139
+            hashes: false, // Do not add random string at the end of the font file names (used to avoid caching when changing version)
             stylesheet: 'less',
             engine: 'node',
-            //ie7: true, //Commented otherwise Webstorm cannot generate css from less
+            // ie7: true, // Commented otherwise Webstorm cannot generate css from less
             fontHeight: 1024,
             descent: 128,
             ascent: 896,
-            startCodepoint: 0xF101, //http://www.fileformat.info/info/unicode/block/private_use_area/utf8test.htm
+            startCodepoint: 0xF101, // http://www.fileformat.info/info/unicode/block/private_use_area/utf8test.htm
             codepoints: {
-                //TODO: maybe we should specify codepoints for all icons otherwise adding new icons might change the correspondence
-                'star': 0x2605, //http://www.fileformat.info/info/unicode/char/2605/index.htm
-                'star-o': 0x2606 //http://www.fileformat.info/info/unicode/char/2606/index.htm
+                // TODO: maybe we should specify codepoints for all icons otherwise adding new icons might change the correspondence
+                'star': 0x2605, // http://www.fileformat.info/info/unicode/char/2605/index.htm
+                'star-o': 0x2606 // http://www.fileformat.info/info/unicode/char/2606/index.htm
             },
             templateOptions: {
                 baseClass: 'kf',
